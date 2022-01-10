2996fd799a30527e9b84d5064ca2fb962e0ef8b2
LuukvH <laj@home.nl>
2022-01-10 21:54:58 +0100

    feat: Remove cors from app

--- src/Services/ChildManagement/KDVManager.Services.ChildManagement.Api/Startup.cs
+++ src/Services/ChildManagement/KDVManager.Services.ChildManagement.Api/Startup.cs
@@ -62,13 +62,6 @@ namespace KDVManager.Services.ChildManagement.Api
 
             app.UseRouting();
 
-            // global cors policy
-            app.UseCors(x => x
-                .AllowAnyMethod()
-                .AllowAnyHeader()
-                .SetIsOriginAllowed(origin => true) // allow any origin
-                .AllowCredentials()); // allow credentials
-
             app.UseCustomExceptionHandler();
 
             app.UseAuthorization();

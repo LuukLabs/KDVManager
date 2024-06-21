9468c4c6b5f7101cc44b8ddfd838c0e1fe17a9d4
LuukvH <luuk@drieam.com>
2024-06-21 20:56:15 +0200

    fix: Crash when opening modals
    The modals were outside the query client provider, this made the application crash when opening a modal.

--- src/web/src/components/AuthProviderLayout.tsx
+++ src/web/src/components/AuthProviderLayout.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AuthProviderWithNavigate from "./AuthProviderWithNavigate";
 import MainNavbar from "./MainNavbar";
 import { Outlet } from "react-router-dom";
+import NiceModal from "@ebay/nice-modal-react";
 
 const queryClient = new QueryClient();
 
@@ -9,9 +10,11 @@ function AuthProviderLayout() {
   return (
     <AuthProviderWithNavigate>
       <QueryClientProvider client={queryClient}>
-        <MainNavbar>
-          <Outlet />
-        </MainNavbar>
+        <NiceModal.Provider>
+          <MainNavbar>
+            <Outlet />
+          </MainNavbar>
+        </NiceModal.Provider>
       </QueryClientProvider>
     </AuthProviderWithNavigate>
   );
--- src/web/src/index.tsx
+++ src/web/src/index.tsx
@@ -4,7 +4,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { createRoot } from "react-dom/client";
 import { SnackbarProvider } from "notistack";
-import NiceModal from "@ebay/nice-modal-react";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider/LocalizationProvider";
@@ -19,9 +18,7 @@ root.render(
     <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="nl">
       <I18nextProvider i18n={i18n}>
         <SnackbarProvider>
-          <NiceModal.Provider>
-            <App />
-          </NiceModal.Provider>
+          <App />
         </SnackbarProvider>
       </I18nextProvider>
     </LocalizationProvider>

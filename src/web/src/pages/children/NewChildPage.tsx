4d4a071bbba624e38f3b6e6ce6be9638cbef6f55
LuukvH <luuk@drieam.com>
2024-06-25 19:22:47 +0200

    chore: Move container to layout
    Moved the container wrapper to the layout to keep the pages as consistent as possible and prevent duplication.

--- src/web/src/pages/children/NewChildPage.tsx
+++ src/web/src/pages/children/NewChildPage.tsx
@@ -1,4 +1,3 @@
-import Container from "@mui/material/Container";
 import { type CreateChildCommand } from "@api/models/createChildCommand";
 import { Controller, useForm } from "react-hook-form";
 import { FormContainer, TextFieldElement } from "react-hook-form-mui";
@@ -36,40 +35,38 @@ const NewChildPage = () => {
 
   return (
     <>
-      <Container>
-        <div className="t1">{t("Single user Creation")}</div>
-        <Paper style={{ padding: 16 }}>
-          <FormContainer formContext={formContext} handleSubmit={handleSubmit(onSubmit)}>
-            <Grid container alignItems="flex-start" spacing={2}>
-              <Grid item xs={12}>
-                <TextFieldElement name="givenName" label={t("Voornaam")} required fullWidth />
-              </Grid>
-              <Grid item xs={12}>
-                <TextFieldElement name="familyName" label={t("Achternaam")} required fullWidth />
-              </Grid>
-              <Grid item xs={12}>
-                <Controller
-                  control={formContext.control}
-                  name="dateOfBirth"
-                  render={({ field }) => {
-                    return (
-                      <DatePicker
-                        label={t("Date")}
-                        defaultValue={field.value ? dayjs(field.value) : undefined}
-                        inputRef={field.ref}
-                        onChange={(date) => {
-                          field.onChange(date);
-                        }}
-                      />
-                    );
-                  }}
-                ></Controller>
-              </Grid>
+      <div className="t1">{t("Single user Creation")}</div>
+      <Paper style={{ padding: 16 }}>
+        <FormContainer formContext={formContext} handleSubmit={handleSubmit(onSubmit)}>
+          <Grid container alignItems="flex-start" spacing={2}>
+            <Grid item xs={12}>
+              <TextFieldElement name="givenName" label={t("Voornaam")} required fullWidth />
             </Grid>
-            <Button onClick={handleSubmit(onSubmit)}>{t("Save", { ns: "common" })}</Button>
-          </FormContainer>
-        </Paper>
-      </Container>
+            <Grid item xs={12}>
+              <TextFieldElement name="familyName" label={t("Achternaam")} required fullWidth />
+            </Grid>
+            <Grid item xs={12}>
+              <Controller
+                control={formContext.control}
+                name="dateOfBirth"
+                render={({ field }) => {
+                  return (
+                    <DatePicker
+                      label={t("Date")}
+                      defaultValue={field.value ? dayjs(field.value) : undefined}
+                      inputRef={field.ref}
+                      onChange={(date) => {
+                        field.onChange(date);
+                      }}
+                    />
+                  );
+                }}
+              ></Controller>
+            </Grid>
+          </Grid>
+          <Button onClick={handleSubmit(onSubmit)}>{t("Save", { ns: "common" })}</Button>
+        </FormContainer>
+      </Paper>
     </>
   );
 };

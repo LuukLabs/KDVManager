a813fe04beb3077e1fa49a4c802a9279c8c6c21d
LuukvH <luuk@drieam.com>
2024-07-20 22:20:14 +0200

    feat: Improve crumbs translations
    Move the translation of the crumbs inside the router.
    This will make sure translations can be correctly extracted
    from the source.

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -1,70 +1,87 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 import ErrorPage from "./components/ErrorPage";
 import MainLayout from "./components/AuthProviderLayout";
+import { type TFunction } from "i18next";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout />,
-    errorElement: <ErrorPage />,
-    handle: {
-      crumb: () => "Home",
-    },
-    children: [
-      {
-        path: "children/:childId",
-        lazy: () => import("./pages/children/UpdateChildPage"),
-      },
-      {
-        path: "children",
-        lazy: () => import("./pages/children/IndexChildPage"),
-        handle: {
-          crumb: () => "Children",
+const router = (t: TFunction<"translation", undefined>) =>
+  createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      errorElement: <ErrorPage />,
+      handle: {
+        crumb: () => {
+          return t("Home");
         },
       },
-      {
-        path: "people",
-        lazy: () => import("./pages/people/IndexPersonPage"),
-        handle: {
-          crumb: () => "People",
+      children: [
+        {
+          path: "children/:childId",
+          lazy: () => import("./pages/children/UpdateChildPage"),
         },
-      },
-      {
-        path: "children/new",
-        lazy: () => import("./pages/children/NewChildPage"),
-      },
-      {
-        path: "groups",
-        lazy: () => import("./pages/groups/ListGroupsPage"),
-        handle: {
-          crumb: () => "Groups",
-        },
-      },
-      {
-        path: "settings",
-        handle: {
-          crumb: () => "Settings",
-        },
-        children: [
-          {
-            index: true,
-            lazy: () => import("./pages/settings/SettingsPage"),
-          },
-          {
-            path: "scheduling",
-            lazy: () => import("./pages/settings/SchedulingSettingsPage"),
-            handle: {
-              crumb: () => "Scheduling",
+        {
+          path: "children",
+          lazy: () => import("./pages/children/IndexChildPage"),
+          handle: {
+            crumb: () => {
+              return t("Children");
             },
           },
-        ],
-      },
-    ],
-  },
-]);
+        },
+        {
+          path: "people",
+          lazy: () => import("./pages/people/IndexPersonPage"),
+          handle: {
+            crumb: () => {
+              return t("People");
+            },
+          },
+        },
+        {
+          path: "children/new",
+          lazy: () => import("./pages/children/NewChildPage"),
+        },
+        {
+          path: "groups",
+          lazy: () => import("./pages/groups/ListGroupsPage"),
+          handle: {
+            crumb: () => {
+              return t("Groups");
+            },
+          },
+        },
+        {
+          path: "settings",
+          handle: {
+            crumb: () => {
+              return t("Settings");
+            },
+          },
+          children: [
+            {
+              index: true,
+              lazy: () => import("./pages/settings/SettingsPage"),
+            },
+            {
+              path: "scheduling",
+              lazy: () => import("./pages/settings/SchedulingSettingsPage"),
+              handle: {
+                crumb: () => {
+                  return t("Scheduling");
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
+  ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  const { t } = useTranslation();
+
+  return <RouterProvider router={router(t)} />;
 }
 
 export default App;

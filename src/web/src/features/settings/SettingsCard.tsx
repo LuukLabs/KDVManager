cac9a02a004f010c8adc7106c60cc4add66f5997
LuukvH <luuk@drieam.com>
2024-07-20 09:20:40 +0200

    feat: Create pages for scheduling settings page
    Added the pages and cards for the scheduling settings page.

---
+++ src/web/src/features/settings/SettingsCard.tsx
@@ -0,0 +1,49 @@
+import { useNavigate } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import CardActionArea from "@mui/material/CardActionArea";
+import Box from "@mui/material/Box";
+import { type ReactNode } from "react";
+
+type SettingsCardProps = {
+  title: string;
+  description: string;
+  navigateTo: string;
+  icon: ReactNode;
+};
+
+export const SettingsCard: React.FC<SettingsCardProps> = ({
+  title,
+  description,
+  navigateTo,
+  icon,
+}) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(navigateTo);
+  };
+
+  return (
+    <Card>
+      <CardActionArea onClick={handleClick}>
+        <CardContent>
+          <Box display="flex" alignItems="center">
+            {icon}
+            <Box ml={2}>
+              <Typography variant="h5" component="div">
+                {title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {description}
+              </Typography>
+            </Box>
+          </Box>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default SettingsCard;
--- src/web/src/pages/settings/SettingsPage.tsx
+++ src/web/src/pages/settings/SettingsPage.tsx
@@ -1,13 +1,36 @@
-import { Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
+import { SettingsCard } from "../../features/settings/SettingsCard";
+import Grid from "@mui/material/Grid";
+import CalendarMonth from "@mui/icons-material/CalendarMonth";
+import React from "react";
 
 const SettingsPage = () => {
   const { t } = useTranslation();
 
+  const settings = [
+    {
+      title: t("Scheduling"),
+      description: t("Manage groups and timeslots."),
+      navigateTo: "/settings/scheduling",
+      icon: <CalendarMonth fontSize="large" />,
+    },
+  ];
+
   return (
-    <>
-      <Typography variant="h2">{t("Settings")}</Typography>
-    </>
+    <React.Fragment>
+      <Grid container spacing={2} sx={{ marginTop: "5px" }}>
+        {settings.map((setting, index) => (
+          <Grid item xs={12} sm={6} md={3} key={index}>
+            <SettingsCard
+              title={setting.title}
+              description={setting.description}
+              navigateTo={setting.navigateTo}
+              icon={setting.icon}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </React.Fragment>
   );
 };
 

5cca37472ef7034f596c48e48a4ac6c8a4afc927
LuukvH <luuk@drieam.com>
2025-06-16 22:49:55 +0200

    refactor: Instead of hooks use interceptor approach

--- src/web/src/components/AuthProviderLayout.tsx
+++ src/web/src/components/AuthProviderLayout.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AuthProviderWithNavigate from "./AuthProviderWithNavigate";
+import AuthInject from "./AuthInject";
 import MainNavbar from "./MainNavbar";
 import { Outlet } from "react-router-dom";
 import NiceModal from "@ebay/nice-modal-react";
@@ -9,6 +10,7 @@ const queryClient = new QueryClient();
 function AuthProviderLayout() {
   return (
     <AuthProviderWithNavigate>
+      <AuthInject />
       <QueryClientProvider client={queryClient}>
         <NiceModal.Provider>
           <MainNavbar>

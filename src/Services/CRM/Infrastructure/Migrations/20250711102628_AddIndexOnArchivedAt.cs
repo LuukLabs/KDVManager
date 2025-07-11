eac9fa8275fb00c82d70fc335ad56e0e656c5b9c
LuukvH <luuk@drieam.com>
2025-07-11 12:28:37 +0200

    feat: Add index on ArchivedAt column in Children table

---
+++ src/Services/CRM/Infrastructure/Migrations/20250711102628_AddIndexOnArchivedAt.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KDVManager.Services.CRM.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexOnArchivedAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                            name: "IX_Children_ArchivedAt",
+                            table: "Children",
+                            column: "ArchivedAt"
+                        );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                           name: "IX_Children_ArchivedAt",
+                           table: "Children"
+                       );
+        }
+    }
+}

cf43385c67ff66a8c284624f5a33eb4b248f3251
LuukvH <luuk@drieam.com>
2025-07-29 17:04:01 +0200

    fix: Correct interface declaration to type

--- src/web/src/features/closurePeriods/DeleteClosurePeriodButton.tsx
+++ src/web/src/features/closurePeriods/DeleteClosurePeriodButton.tsx
@@ -6,7 +6,7 @@ import { useTranslation } from "react-i18next";
 import { useSnackbar } from "notistack";
 import { useQueryClient } from "@tanstack/react-query";
 
-interface DeleteClosurePeriodButtonProps {
+type DeleteClosurePeriodButtonProps = {
   id: string;
 }
 
